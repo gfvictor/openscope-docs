a593f8bef70928e98b7553fef4b48c3091781f50
Victor Farias <gfvictor@proton.me>
2026-07-16 20:46:53 +0900

    chore: add allowed dev origins for local network mobile testing

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@ import { createMDX } from 'fumadocs-mdx/next'
 
 const withMDX = createMDX()
 
-/** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  allowedDevOrigins: ['192.168.11.7'],
 }
 
 export default withMDX(config)
